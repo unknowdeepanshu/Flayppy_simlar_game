dd69225ce8cc2aa5cc26ffd5ca6de993d9afa995
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2025-08-20 23:40:00 +0530

    this change in index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require("express");
 const app=express();
-const port=8080;
+const port= process.env.PORT || 8080;
 const path=require("path")
 app.listen(port,()=>{
     console.log(`Server running at http://localhost:${port}`);
