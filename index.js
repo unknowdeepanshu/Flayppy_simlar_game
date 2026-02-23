6efed5c77f2dbfa951fab7b089ee1f7096151114
unknowdeepanshu <dipanshuvishwakarma616@gmail.com>
2026-02-24 03:23:21 +0530

    add restart features and add src file to separated  the code and images

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-const express=require("express");
-const app=express();
-const port= process.env.PORT || 8080;
-const path=require("path")
-app.listen(port,()=>{
-    console.log(`Server running at http://localhost:${port}`);
-})
-app.use(express.static(path.join(__dirname,"public")));
-app.use("/phaser",express.static(path.join(__dirname,"node_modules","phaser","dist")))
-app.use("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"/index.html"))
-})
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8080;
+const path = require("path");
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(express.static(path.join(__dirname, "src")));
+app.use(
+  "/phaser",
+  express.static(path.join(__dirname, "node_modules", "phaser", "dist")),
+);
---
+++ src/js/game.js
@@ -0,0 +1,16 @@
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: [Scenc2, Scenc1],
+  backgroundColor: "#eeeeee",
+  Physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+};
+new Phaser.Game(config);
+// console.log("Game is starting");
